32178dc723a165a482c5e18c90e47c3b084ccd0a
zechu <zechu.miao@js.design>
2025-04-21 16:39:16 +0800

    feat: project初始化

---
+++ vite/plugins/index.js
@@ -0,0 +1,19 @@
+import vue from '@vitejs/plugin-vue'
+
+import createAutoImport from './auto-import'
+import createSvgIcon from './svg-icon'
+import createCompression from './compression'
+import createSetupExtend from './setup-extend'
+import { codeInspectorPlugin } from 'code-inspector-plugin';
+
+export default function createVitePlugins(viteEnv, isBuild = false) {
+    const vitePlugins = [vue()]
+    vitePlugins.push(createAutoImport())
+	vitePlugins.push(createSetupExtend())
+    vitePlugins.push(createSvgIcon(isBuild))
+    vitePlugins.push(codeInspectorPlugin({
+        bundler: 'vite',
+      }))
+	isBuild && vitePlugins.push(...createCompression(viteEnv))
+    return vitePlugins
+}
